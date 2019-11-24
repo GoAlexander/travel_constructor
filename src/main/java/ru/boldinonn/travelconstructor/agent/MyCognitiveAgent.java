bc7f26271c5508861a15230f0105d749fee79547
GoAlexander <AlexanderDydychkin@yandex.ru>
2019-11-24 14:06:44 +0300

    [jena] Introduce jena (buggy version)

--- src/main/java/ru/boldinonn/travelconstructor/agent/MyCognitiveAgent.java
+++ src/main/java/ru/boldinonn/travelconstructor/agent/MyCognitiveAgent.java
@@ -19,21 +19,22 @@ public class MyCognitiveAgent extends CognitiveAgent {
     //protected String internalOnt = "http://x556uq/RequirementsOfOrderManagerToDelivery-ootos.owl";
     //protected String ftpNameOfOntFile = "RequirementsOfOrderManagerToDelivery-ootos.owl";
 
-    static Config myConfig = new Config("MyCognitiveAgent");  //name of config file // you can edit it :)
-    protected static final String internalOnt = myConfig.getProperty("internalOnt");
-    protected static String serviceName = myConfig.getProperty("serviceName");
+    //static Config myConfig = new Config("MyCognitiveAgent");  //name of config file // you can edit it :)
+    //protected static final String internalOnt = myConfig.getProperty("internalOnt");
+    //protected static String serviceName = myConfig.getProperty("serviceName");
 
 
     public static void main(String[] args) {
         MyCognitiveAgent d = new MyCognitiveAgent();
         //d.setup();
         KbRequest myKbRequest = new KbRequest();
-        myKbRequest.setOntology(internalOnt);
+        //myKbRequest.setOntology(internalOnt);
+        myKbRequest.setOntology("/home/alexander/Files/Workspaces/IdeaProjects/tmp/boldino.owl");
         //System.out.println(myKbRequest.processMessage("Termobox001 instanceOf FoodTermobox", "INFORM", "factTrue"));
-        System.out.println(myKbRequest.processMessage("Termobox001 instanceOf FoodTermobox", "QUERY_IF", "factTrue"));
-        System.out.println(myKbRequest.processMessage("MedicalTermobox instanceOf Termobox", "QUERY_IF", "factTrue"));
-        //System.out.println(myKbRequest.processMessage("MedicalTermobox", "QUERY_IF", "factTrue"));
-        //System.out.println(myKbRequest.processMessage("Termobox", "QUERY_IF", "factTrue"));
+        //System.out.println(myKbRequest.processMessage("Termobox001 instanceOf FoodTermobox", "QUERY_IF", "factTrue"));
+        //System.out.println(myKbRequest.processMessage("MedicalTermobox instanceOf Termobox", "QUERY_IF", "factTrue"));
+        System.out.println(myKbRequest.processMessage("Boldino-Service", "QUERY_IF", "factTrue"));
+        System.out.println(myKbRequest.processMessage("Service001-excursion-t8-ex002 instanceOf Boldino-Service", "QUERY_IF", "factTrue"));
     }
 
 }
