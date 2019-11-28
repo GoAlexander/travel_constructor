fe4b194a12e613078307d06b6ddc54f79a4a5d3c
GoAlexander <AlexanderDydychkin@yandex.ru>
2019-11-28 23:30:56 +0300

    Clean code and repository

--- src/main/java/ru/boldinonn/travelconstructor/agent/MyCognitiveAgent.java
+++ src/main/java/ru/boldinonn/travelconstructor/agent/MyCognitiveAgent.java
@@ -10,29 +10,26 @@
 
 package ru.boldinonn.travelconstructor.agent;
 
-public class MyCognitiveAgent extends CognitiveAgent {
-    //Test merging:
-    //protected String internalOnt = "http://x556uq/C-fact-Delivery-ootos.owl";
-    //protected String ftpNameOfOntFile = "C-fact-Delivery-ootos.owl";
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
-    //protected String internalOnt = "http://x556uq/RequirementsOfOrderManagerToDelivery-ootos.owl";
-    //protected String ftpNameOfOntFile = "RequirementsOfOrderManagerToDelivery-ootos.owl";
-
-    //static Config myConfig = new Config("MyCognitiveAgent");  //name of config file // you can edit it :)
-    //protected static final String internalOnt = myConfig.getProperty("internalOnt");
-    //protected static String serviceName = myConfig.getProperty("serviceName");
-
+public class MyCognitiveAgent {
 
     public static void main(String[] args) {
-        //MyCognitiveAgent d = new MyCognitiveAgent();
-        //d.setup();
+
+        //FIXME!
+        Set<String> artifactoryLoggers = new HashSet<>(Arrays.asList("org.apache.http", "groovyx.net.http", "org.apache.jena.shared.LockMRSW", "org.apache.jena.riot.system.stream.StreamManager", "org.apache.jena.util.FileManager"));
+        for(String log:artifactoryLoggers) {
+            ch.qos.logback.classic.Logger artLogger = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(log);
+            artLogger.setLevel(ch.qos.logback.classic.Level.INFO);
+            artLogger.setAdditive(false);
+        }
+
         KbRequest myKbRequest = new KbRequest();
-        //myKbRequest.setOntology(internalOnt);
-        myKbRequest.ontPath = "src/main/resources/public/boldino.owl";
+        myKbRequest.ontPath = "src/main/resources/public/boldino.owl"; //FIXME!
         myKbRequest.setOntology("http://localhost:8080/boldino.owl");
         //System.out.println(myKbRequest.processMessage("Termobox001 instanceOf FoodTermobox", "INFORM", "factTrue"));
-        //System.out.println(myKbRequest.processMessage("Termobox001 instanceOf FoodTermobox", "QUERY_IF", "factTrue"));
-        //System.out.println(myKbRequest.processMessage("MedicalTermobox instanceOf Termobox", "QUERY_IF", "factTrue"));
         System.out.println("PRINT:" + myKbRequest.processMessage("Boldino-Service", "QUERY_IF", "factTrue"));
         //System.out.println(myKbRequest.processMessage("Service001-excursion-t8-ex002 instanceOf Boldino-Service", "QUERY_IF", "factTrue"));
         //System.out.println(myKbRequest.processMessage("Service001-excursion-t8-ex002 instanceOf Boldino-Service", "QUERY_IF", "factTrue"));
