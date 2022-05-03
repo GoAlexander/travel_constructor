67f4609122b5ea7ab4228e8067ad16d9d893ecb5
Maxim Teterevenkov <mteterevenkov@yandex.ru>
2022-05-03 22:44:04 +0300

    Add initial version of frontend

--- src/main/java/ru/boldinonn/travelconstructor/web/ApiController.java
+++ src/main/java/ru/boldinonn/travelconstructor/web/ApiController.java
@@ -5,17 +5,54 @@ import org.springframework.web.bind.annotation.*;
 import ru.boldinonn.travelconstructor.agent.KbRequest;
 import ru.boldinonn.travelconstructor.model.Service;
 
+import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
 public class ApiController {
+
+    private static final List<String> providers = Arrays.asList("Музей Болдино", "Столовая", "Полёты на шаре", "Костюмированные шоу");
+
+    private static final Map<String, List<ServiceOfProvider>> servicesMap = initMap();
+
+    private static Map<String, List<ServiceOfProvider>> initMap() {
+        Map<String, List<ServiceOfProvider>> map = new HashMap<>();
+        ServiceOfProvider museumTicket = new ServiceOfProvider("Экскурсия «Усадьба Пушкиных в Болдине»", 1000,
+                Arrays.asList(
+                        OffsetDateTime.parse("2022-05-03T10:00:00+03:00"),
+                        OffsetDateTime.parse("2022-05-03T12:00:00+03:00"))
+        );
+
+        ServiceOfProvider photo = new ServiceOfProvider("Фотосессия 1 час", 2500,
+                Arrays.asList(
+                        OffsetDateTime.parse("2022-05-03T10:00:00+03:00"),
+                        OffsetDateTime.parse("2022-05-03T11:00:00+03:00"),
+                        OffsetDateTime.parse("2022-05-03T15:00:00+03:00"))
+        );
+
+        ServiceOfProvider gift = new ServiceOfProvider("Экскурсия «В мире литературных героев «Повестей Белкина»", 1200,
+                Arrays.asList(
+                        OffsetDateTime.parse("2022-05-03T10:00:00+03:00"),
+                        OffsetDateTime.parse("2022-05-03T11:00:00+03:00"),
+                        OffsetDateTime.parse("2022-05-03T14:00:00+03:00"))
+        );
+
+        map.put(providers.get(0), Arrays.asList(museumTicket, photo, gift));
+
+        return map;
+    }
 
     @Autowired
     private KbRequest kbRequest;
 
     @GetMapping
     public String hello() {
-        return "Hello from Travel constructor";
+        return "Hello from Travel constructor API";
     }
 
     @GetMapping("/processMessage")
@@ -23,9 +60,15 @@ public class ApiController {
         return kbRequest.processMessage(message, performative, type);
     }
 
-    @GetMapping("/listServicesOf")
-    public List<String> listServicesOf(@RequestParam String superClass) {
-        return kbRequest.listServicesOf(superClass);
+    @GetMapping("/providers")
+    public List<String> getProviders() {
+        return providers; 
+    }
+
+    @GetMapping("/servicesOf/{provider}")
+    public List<ServiceOfProvider> getServicesOf(@PathVariable(value = "provider") String provider) {
+        return servicesMap.get(provider);
+        //return kbRequest.listServicesOf(provider);
     }
 
     @PostMapping("/service")
